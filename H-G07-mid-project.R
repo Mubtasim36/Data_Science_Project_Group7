8ce4e344190ad2d555c68eada82911e97b68e224
Mubtasim36 <22-49002-3@student.aiub.edu>
2025-11-19 19:28:55 +0600

    Till ScatterPlot

--- H-G07-mid-project.R
+++ H-G07-mid-project.R
@@ -25,37 +25,21 @@ cat(
 )
 
 
-#5.Generate basic descriptive statistics 1
+#5.Generate basic descriptive statistics
 
-stats <- function(employee) {
-  
-  num_col <- employee[, sapply(employee, is.numeric)]
-  
-  get_mode <- function(x) {
-    freq_table <- table(x)
-    names(freq_table)[which.max(freq_table)]
-  }
-  
-  statistics <- data.frame(  #using na.rm to remove NA from calculations
-    Mean = sapply(num_col, mean, na.rm = TRUE),                  #sapply(matrix or dataframe,function to apply)
-    Median = sapply(num_col, median, na.rm = TRUE),
-    Mode = sapply(num_col, get_mode),
-    Std_Deviation = sapply(num_col, sd, na.rm = TRUE),
-    Minimum = sapply(num_col, min, na.rm = TRUE),
-    Maximum = sapply(num_col, max, na.rm = TRUE),
-    Count = sapply(num_col, length)
-  )  
-  
-  return(statistics)
-}
-
-stats(employee)
-
-
-
-#5.descriptive stat using summary()
 summary(employee)
+library(dplyr)
 
+#Generate basic descriptive statistics for Age (by using this method) 
+employee %>%
+  
+  summarise(
+    count = n(),
+    mean_Age = mean(employee$Age),
+    sd_Age = sd(employee$Age),
+    mean_Age = mean(employee$Age),
+    sd_Age = sd(employee$Age)
+  )
 
 
 #6.Identify categorical and numerical features
@@ -68,6 +52,95 @@ sapply(employee, function(col) {
 })
 
 
-#B. Data Exploration & Visualization
+#B.1 Data Exploration & Visualization
+#Creating Histogram for Each Numerical Data
+hist(employee$Age)
+hist(employee$Experience_Years)
+hist(employee$Performance_Score)
 
 
+#Creating 3 Box-plot for Each Numerical Data
+
+ggplot(employee, aes(y = Experience_Years)) +
+  geom_boxplot(fill = "blue") +
+  labs(title = "Boxplot of Experience_Years", y = "Experience_Years")
+
+ggplot(employee, aes(y = Age)) +
+  geom_boxplot(fill = "green") +
+  labs(title = "Boxplot of Age", y = "Age")
+
+
+ggplot(employee, aes(y = Performance_Score)) +
+  geom_boxplot(fill = "red") +
+  labs(title = "Boxplot of Performance_Score", y = "Performance_Score")
+
+
+#Creating 3 Bar-chart for Each Numerical Data
+
+#Bar chart for Employee Age 
+Age_counts <- table(employee$Age)
+barplot(Age_counts,
+        main = "Age Distribution",
+        xlab = "Age",
+        ylab = "Count",
+        col = "blue",
+        ylim = c(0, 1.2 * max(Age_counts)),#limit for y axis values
+        las = 2)
+
+#Bar chart for Employee Gender 
+
+Gender_counts <- table(employee$Gender)
+barplot(Gender_counts,
+        main = "Gender Distribution",
+        xlab = "Gender",
+        ylab = "Count",
+        col = "yellow",
+        ylim = c(0, 1.2 * max(Gender_counts)), #limit for y axis values
+        las = 2)
+
+
+#Bar chart for Employee Department 
+dept_counts <- table(employee$Department)
+barplot(dept_counts,
+        main = "Department Distribution",
+        xlab = "Department",
+        ylab = "Count",
+        col = "red",
+        ylim = c(0, 1.2 * max(dept_counts)),  #limit for y axis values
+        las = 2)
+
+
+
+#Frequency of categorical variables
+table(employee$Gender)
+table(employee$Department)
+ 
+
+
+#B.2
+#Bivariate Analysis
+#Correlation matrix (heatmap)
+
+install.packages("reshape2")
+
+
+library(reshape2)
+
+num_data <- employee[sapply(employee, is.numeric)]
+corr_mat <- cor(num_data)
+ 
+melted_corr_mat <- melt(corr_mat)       #Convert matrix into long format
+
+library(ggplot2)
+ggplot(data = melted_corr_mat, aes(x=Var1, y=Var2,fill=value)) + 
+geom_tile()+                       #to draw the colored tiles for correlation values.
+ggtitle("Heatmap for Correlation")          #for heatmap title
+
+
+
+#Scatterplot
+
+ggplot(employee, aes(x = Salary, y = Performance_Score )) +
+  geom_point() +     #Adds scatter plot points (one point for each employee).
+  geom_smooth(method = "lm", se = FALSE, color = "blue") +     #Adds a linear regression line ("lm" = linear model).se = FALSE removes the shaded confidence interval.
+  labs(title = "Salary vs PS", x = "Salary", y = "Performance_Score ")
